5982bbdadda5cedefe3921623f20534cefe2ad7c
Giuseppe Li Gioi <pepe2117k@gmail.com>
2026-01-28 12:33:23 +0100

    added. logic for filter results in useRecipeSearch

--- src/hooks/useRecipesSearch.ts
+++ src/hooks/useRecipesSearch.ts
@@ -4,9 +4,37 @@ import { useState, useEffect } from "react";
 
 export function useRecipeSearch(){
 const [query, setQuery] = useState<string>("")
+const [selectedCategories, setSelectedCategories] = useState<string[]>([])
 const [results, setResults] = useState<Recipe[]>(recipes)
 
+useEffect(() => {
+getFilteredRecipes(query, selectedCategories, recipes)
+}, [query, selectedCategories])
+
+
+const getFilteredRecipes = (q: string, categories: string[], recipes: Recipe[]) => {
+if(categories.length === 0 && !q) return;
+if(categories.length === 0 && q){
+setResults(filterByQuery(q, recipes))
+}else{
+let filtered: Recipe[] = filterByCategories(categories, recipes)
+setResults(filterByQuery(q, filtered))
+}
+}
+
+const filterByQuery = (q: string, recipes: Recipe[]) : Recipe[]=> {
+if(!q) return recipes;
+const filtered : Recipe[] = recipes.filter((r) => r.title.toLowerCase().trim().includes(q.trim().toLowerCase()))
+return filtered
+}
+
+const filterByCategories = (categories: string[], recipes: Recipe[]) : Recipe[] => {
+    if(categories.length === 0) return recipes;
+    const filtered = recipes.filter((r) => categories.includes(r.category.trim().toLowerCase()))
+    return filtered 
+}
+
 return {
-    query, results, setQuery
+    query, results, setQuery, getFilteredRecipes, setSelectedCategories
 }
 }
