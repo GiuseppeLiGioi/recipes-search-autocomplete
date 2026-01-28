59796324d9223caff35a13f1dbff864ef5837518
Giuseppe Li Gioi <pepe2117k@gmail.com>
2026-01-28 19:24:39 +0100

    added. toggle selection of category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,14 +1,26 @@
 import { useCategories } from "../../contexts/RecipesContext";
 
 type CategoryProps = {
-title: string
-}
+  title: string;
+};
 
-export default function Category({title} : CategoryProps){
-const {setSelectedCategories} = useCategories()
-return(
-<div className="container-category">
-<button className="btn-category" onClick={() => setSelectedCategories(prev => [...prev, title.trim().toLowerCase()])}>{title}</button>
-</div>
-)
+export default function Category({ title }: CategoryProps) {
+  const { setSelectedCategories } = useCategories();
+  const normalizedTitle = title.trim().toLowerCase();
+  return (
+    <div className="container-category">
+      <button
+        className="btn-category"
+        onClick={() =>
+          setSelectedCategories((prev) =>
+            prev.includes(normalizedTitle)
+              ? prev.filter((cat) => cat !== normalizedTitle)
+              : [...prev, normalizedTitle],
+          )
+        }
+      >
+        {title}
+      </button>
+    </div>
+  );
 }
